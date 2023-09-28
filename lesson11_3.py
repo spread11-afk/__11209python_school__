fcdd51e68128b314329cdefe6370ffbd675f5a87
spread11 <yyhhnnjj@gmail.com>
2023-09-28 13:43:52 +0800

    modify

---
+++ lesson11_3.py
@@ -0,0 +1,21 @@
+class Person:
+    def __init__(self,name,weight,height) -> None:
+        self.name = name
+        self.weight = weight
+        self.height = height
+
+    #method
+    def bmi(self) -> float:
+        return round(self.weight / (self.height / 100) ** 2,ndigits=2)
+
+
+
+    def __str__(self) -> str:
+        return f"name = {self.name}\nweight = {self.weight}\nheight = {self.height}"
+    
+
+
+if __name__ == '__main__':
+    p1 = Person("rebert",78,183)
+    print(p1)
+    print(p1.bmi())
