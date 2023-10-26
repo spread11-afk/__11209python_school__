b801cefae6aa74099c67c4eb050173527600f5a4
spread11 <yyhhnnjj@gmail.com>
2023-10-26 11:47:03 +0800

    1

---
+++ youbike/datasource_test.py
@@ -0,0 +1,45 @@
+import requests
+import sqlite3
+
+
+def __download_youbike_data()->list[dict]:
+    '''
+    下載youbike資料2.0
+    https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json
+    '''
+    youbike_url = 'https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json'
+    response = requests.get(youbike_url)
+    response.raise_for_status()
+    print('Success')
+    return response.json()
+
+def __create_table(con):
+	cursor = con.cursor()
+	cursor.execute(
+		'''
+		CREATE TABLE   IF NOT EXISTS 台北市youbike(
+			"id"	INTEGER,
+			"站點名稱"	TEXT NOT NULL,
+			"行政區"	TEXT NOT NULL,
+			"更新時間"	TEXT NOT NULL,
+			"地址"	TEXT,
+			"總車輛數"	INTEGER,
+			"可借"	INTEGER,
+			"可還"	INTEGER,
+			'測試'	INTEGER,
+			PRIMARY KEY("id" AUTOINCREMENT)
+		);
+
+		'''
+	)
+	con.commit()
+
+def update_data_to_sqlite():
+    '''下載、更新資料'''
+    data = __download_youbike_data()
+    conn = sqlite3.connect('youtube.db')
+    __create_table(conn)
+    
+    
+    #for item in data:
+        #print(item)
--- youbike/test.py
+++ youbike/test.py
@@ -1,26 +1,29 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
 import datasource
+from tkinter import messagebox
+
 
 class Window(tk.Tk):
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs) -> None:
         super().__init__(**kwargs)
         try:
-            datasource.updata_sqlite_data()
+            datasource.download_youbike_data()
         except Exception:
-            messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
+            messagebox.showerror('Error','關閉視窗\n稍後再試')
             self.destroy()
 
+
         
-        
+
 
 def main():
     window = Window()
-    window.title('台北市youbike2.0')
+    window.title('youbike_ver2.0')
     window.geometry('600x300')
-    window.resizable(width=False,height=False)    
+    window.resizable(width=False,height=False)
     window.mainloop()
+
 
 if __name__ == '__main__':
     main()
