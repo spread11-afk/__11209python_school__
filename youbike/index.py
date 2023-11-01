79d75738907e90d0a1bbaf346468efa74963168b
spread11 <yyhhnnjj@gmail.com>
2023-11-01 11:28:26 +0800

    modify

--- youbike/index.py
+++ youbike/index.py
@@ -4,7 +4,6 @@ from tkinter import messagebox
 from threading import Timer
 import datasource
 
-
 class Window(tk.Tk):
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
@@ -14,24 +13,21 @@ class Window(tk.Tk):
             messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
             self.destroy()
 
+        print(datasource.lastest_datetime_data())
 
-t = None
 
-def main(): 
-    
-
-    
+def main():    
     def update_data(w:Window)->None:
         datasource.updata_sqlite_data()
-        window.after(10*1000,update_data,w)
-    
+        window.after(3*60*1000,update_data,w)
+          
+
     window = Window()
     window.title('台北市youbike2.0')
     window.geometry('600x300')
-    window.resizable(width=False,height=False)    
-    update_data(window)
+    window.resizable(width=False,height=False)
+    update_data(window)          
     window.mainloop()
-    
 
 if __name__ == '__main__':
     main()
