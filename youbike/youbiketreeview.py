65d0104bd0647c3b4d786d85e89df3c2b200e64f
spread11 <yyhhnnjj@gmail.com>
2023-11-02 14:39:54 +0800

    a

--- youbike/youbiketreeview.py
+++ youbike/youbiketreeview.py
@@ -1,44 +1,50 @@
 from tkinter import ttk
-
+from tkinter.simpledialog import Dialog
 
 class YoubikeTreeView(ttk.Treeview):
-    def __init__(self, parent, **kwargs):
-        super().__init__(parent, **kwargs)
-        # ------設定欄位名稱---------------
-        self.heading('sna', text='站點名稱')
-        self.heading('mday', text='更新時間')
-        self.heading('sarea', text='行政區')
-        self.heading('ar', text='地址')
-        self.heading('tot', text='總車輛數')
-        self.heading('sbi', text='可借')
-        self.heading('bemp', text='可還')
+    def __init__(self,parent,**kwargs):
+        super().__init__(parent,**kwargs)
+        self.parent = parent
+        #------設定欄位名稱---------------
+        self.heading('sna',text='站點名稱')
+        self.heading('mday',text='更新時間')
+        self.heading('sarea',text='行政區')
+        self.heading('ar',text='地址')
+        self.heading('tot',text='總車輛數')
+        self.heading('sbi',text='可借')
+        self.heading('bemp',text='可還')
 
-        # ----------設定欄位寬度------------
-        self.column('sna', minwidth=0)
-        self.column('mday', minwidth=0)
-        self.column('sarea', minwidth=0)
-        self.column('ar', minwidth=0)
-        self.column('tot', minwidth=0)
-        self.column('sbi', minwidth=0)
-        self.column('bemp', minwidth=0)
+        #----------設定欄位寬度------------
+        self.column('sna',width=200)
+        self.column('mday',width=150)
+        self.column('sarea',width=50)
+        self.column('ar',width=300)
+        self.column('tot',width=50)
+        self.column('sbi',width=50)
+        self.column('bemp',width=50)
 
-        # ----------(bind_button1)----------
-        self.bind('<ButtonRelease-1>',self.selectItem)
+        #----------bind button1-------
+        self.bind('<ButtonRelease-1>', self.selectedItem)
 
-
-    def update_content(self, site_datas):
+    def update_content(self,site_datas):
         '''
         更新內容
         '''
+        #清除所有內容
         for i in self.get_children():
             self.delete(i)
+        
+        for index,site in enumerate(site_datas):
+            self.insert('','end',text=f"abc{index}",values=site)
 
-        for site in site_datas:
-            self.insert('', 'end', values=site)
+
+    def selectedItem(self,event):
+        selectedItem = self.focus()
+        print(selectedItem)
+        print(self.item(selectedItem))
+        get_password = GetPassword(self.parent)
+        
 
 
-    def selectItem(self,event):
-        selectItem = self.focus()
-        print(selectItem)
-        print(self.item(selectItem))
-
+class GetPassword(Dialog):
+    pass        
