a0cba45366465c4126f636760463c6aaedc88ee0
spread11 <yyhhnnjj@gmail.com>
2023-11-07 14:06:49 +0800

    map

--- homework/20231102/map.py
+++ homework/20231102/map.py
@@ -1,44 +1,27 @@
 import tkinter as tk
 import tkintermapview as tkmap
-from PIL import Image, ImageTk
+
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.thumbnails = {}
-
-        map_widget = tkmap.TkinterMapView(self, width=800, height=600, corner_radius=0)
+        # 建立地圖
+        map_widget = tkmap.TkinterMapView(self,
+                                          width=800,
+                                          height=600,
+                                          corner_radius=0,
+                                          )
         map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-        marker_1 = map_widget.set_position(25.257887730617984, 121.49401660340506, marker=True)
-        map_widget.set_zoom(13)
-        marker_1.set_text("三芝區")
+        marker_1 = map_widget.set_position(
+            25.25956046590159, 121.5018147, marker=True)  # 台北市位置
+        map_widget.set_zoom(17)  # 設定顯示大小
+        marker_1.set_text("三芝市中心")
+        # marker_1.set_position(48.860381, 2.338594)  # 改變位置
+        # marker_1.delete()刪除
 
-        marker_2 = map_widget.set_marker(25.2604340447434, 121.50276763806208, text="老地方")
-        marker_2.data = {'a': 15, 'b': '紀念館', 'image_path': 'A.jpg'}
-
-        # 使用bind方法绑定点击事件
-        marker_2.bind("<Button-1>", self.show_thumbnail)
-
-    def show_thumbnail(self, event):
-        marker = event.widget
-        data = marker.data
-
-        if 'image_path' in data:
-            image_path = data['image_path']
-            if image_path in self.thumbnails:
-                thumbnail = self.thumbnails[image_path]
-            else:
-                image = Image.open(image_path)
-                image.thumbnail((100, 100))
-                thumbnail = ImageTk.PhotoImage(image)
-
-                self.thumbnails[image_path] = thumbnail
-
-            popup = tk.Toplevel()
-            label = tk.Label(popup, image=thumbnail)
-            label.pack()
-            popup.mainloop()
 
 if __name__ == "__main__":
-    app = Window()
-    app.mainloop()
+    window = Window()
+    window.geometry("800x600")
+    window.title("地圖")
+    window.mainloop()
