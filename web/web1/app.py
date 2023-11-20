1d54f45ea968b19146702fe58cf794e8a4c8fb5c
spread11 <yyhhnnjj@gmail.com>
2023-11-20 04:28:17 +0000

    建立兩個網頁，終端機print(debug)

--- web/web1/app.py
+++ web/web1/app.py
@@ -2,6 +2,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def index():
+    print('debug')
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
