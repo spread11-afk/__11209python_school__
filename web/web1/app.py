8116873ddbbef52d4c9ff94ab7bbd5219a4c5ad7
spread11 <yyhhnnjj@gmail.com>
2023-11-22 03:08:27 +0000

    modify

--- web/web1/app.py
+++ web/web1/app.py
@@ -1,8 +1,8 @@
 from flask import Flask,url_for,render_template
+import pandas as pd
 from auth import auth
 from bs import bootstrap
-import random
-import pandas as pd
+
 
 app = Flask(__name__)
 app.register_blueprint(auth.bp)
@@ -10,12 +10,13 @@ app.register_blueprint(bootstrap.bp)
 
 @app.route('/')
 def index():
-    name = 'spread11'
+    name = "徐國堂"
     dataFrame = get_dataFrame()
     return render_template('index.html',name=name,dataFrame=dataFrame)
 
-
 def get_dataFrame()->pd.DataFrame:
-    data = [['xxx',67,92,85],['yyy',78,97,65],['zzz',92,79,85]]
-
-    return pd.DataFrame(data,columns=['姓名','國文','數學','英文'])
+    data = [['徐國堂',67,92,85],
+            ['王XX',78,97,65],
+            ['李XX',92, 79, 85]]    
+    
+    return pd.DataFrame(data,columns=["姓名","國文","英文","數學"])
--- web/web1/bs/bootstrap.py
+++ web/web1/bs/bootstrap.py
@@ -1,7 +1,15 @@
 from flask import Blueprint,render_template
 
-bp = Blueprint('bs',__name__,url_prefix='/bs')
+bp = Blueprint('bs', __name__, url_prefix='/bs')
 
-@bp.route('/')
+@bp.route("/")
 def index():
-    return render_template('bs/index.html')
+    return render_template("bs/index.html")
+
+@bp.route("/product")
+def product():
+    return render_template("bs/product.html")
+
+@bp.route("/profile")
+def profile():
+    return render_template("bs/profile.html")
