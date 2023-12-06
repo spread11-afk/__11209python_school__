71f6dae00d60017577e7c63da99c0a2ff166062d
spread11 <yyhhnnjj@gmail.com>
2023-12-06 05:54:50 +0000

    a

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -4,6 +4,4 @@ import dash_bootstrap_components as dbc
 
 dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-dash2.layout = html.Div([html.H1("H1!"),html.P("p!"),html.P("p2!")],className='container-lg',style={'backgroundColor':'#666'})
-
-
+dash2.layout = html.Div([html.H1("BootStrap Layout!"),html.P("p!"),html.P("p2!")],className='container-lg',style={'backgroundColor':'#666'})
