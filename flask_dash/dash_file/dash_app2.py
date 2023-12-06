6c193b2604a62c18be2824a69a5309c14ab6deed
spread11 <yyhhnnjj@gmail.com>
2023-12-06 04:03:09 +0000

    新增多個檔案

--- flask_dash/dash_file/dash_app2.py
+++ flask_dash/dash_file/dash_app2.py
@@ -1,11 +1,9 @@
-from dash import Dash, html, dcc, callback, Output, Input
-import plotly.express as px
+from dash import Dash, html
 import pandas as pd
+import dash_bootstrap_components as dbc
 
-dash2 = Dash(requests_pathname_prefix="/dash/app2/")
+dash2 = Dash(requests_pathname_prefix="/dash/app2/",external_stylesheets=[dbc.themes.BOOTSTRAP])
 
-dash2.layout = html.Div([
-    html.H1(children='Dash Layout', style={'textAlign':'center'}),
-    ])
+dash2.layout = html.Div([html.H1("H1!"),html.P("p!"),html.P("p2!")],className='container-lg',style={'backgroundColor':'#666'})
 
 
