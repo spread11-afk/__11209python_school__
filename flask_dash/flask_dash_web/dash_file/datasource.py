cb61a92d0617fd841986f1a137bdd755b6ec93d7
spread11 <yyhhnnjj@gmail.com>
2023-12-13 06:51:59 +0000

    modify

--- flask_dash/flask_dash_web/dash_file/datasource.py
+++ flask_dash/flask_dash_web/dash_file/datasource.py
@@ -1,13 +1,31 @@
 import requests
 import psycopg2
+#import password as pw
+import socket
 import os
 
+myip = socket.gethostbyname(socket.gethostname())
+if '172.17.0.0'<= myip <= '172.17.255.255':
+    from . import password as pw
+    print("本機")
+    DATABASE = pw.DATABASE
+    USER = pw.USER
+    PASSWORD = pw.PASSWORD
+    HOST = pw.HOST
+else:
+    DATABASE = os.environ['DATABASE']
+    USER = os.environ['USER']
+    PASSWORD = os.environ['PASSWORD']
+    HOST = os.environ['HOST']
+     
+
+print(f'我的ip是{myip}')
 
 def lastest_datetime_data()->list[tuple]:
-    conn = psycopg2.connect(database=os.environ['DATABASE'],
-                            user=os.environ['USER'], 
-                            password=os.environ['PASSWORD'],
-                            host=os.environ['HOST'], 
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER, 
+                            password=PASSWORD,
+                            host=HOST, 
                             port="5432")
     cursor = conn.cursor()
     sql = '''
@@ -27,10 +45,10 @@ def lastest_datetime_data()->list[tuple]:
     return rows
 
 def search_sitename(word:str) -> list[tuple]:
-    conn = psycopg2.connect(database=os.environ['DATABASE'],
-                            user=os.environ['USER'], 
-                            password=os.environ['PASSWORD'],
-                            host=os.environ['HOST'], 
+    conn = psycopg2.connect(database=DATABASE,
+                            user=USER, 
+                            password=PASSWORD,
+                            host=HOST, 
                             port="5432")
     cursor = conn.cursor()
     sql = '''
