c3c8c470ab4c2b5d664d910e57b7cfe7cc106b23
spread11 <yyhhnnjj@gmail.com>
2023-12-22 03:54:11 +0000

    modify

--- flask_dash/auth/auth.py
+++ flask_dash/auth/auth.py
@@ -2,6 +2,10 @@ from flask import Blueprint,render_template,request,redirect
 from flask_wtf import FlaskForm
 from wtforms import StringField,SelectField,EmailField,BooleanField,DateField,TextAreaField,PasswordField
 from wtforms.validators import DataRequired,Length,Regexp,Optional,EqualTo
+from werkzeug.security import generate_password_hash, check_password_hash
+import secrets
+from .datasource import insert_data
+import datetime
 
 blueprint_auth = Blueprint('auth', __name__,url_prefix='/auth')
 
@@ -65,8 +69,12 @@ def register():
             isGetEmail = form.isGetEmail.data
             print("接受促銷","接受" if isGetEmail else "不接受")
 
-            uBirthday = form.uBirthday.data
-            print("出生",uBirthday)
+            uBirthday:datetime.date | None= form.uBirthday.data
+            if uBirthday is not None:
+                uBirthday_str = uBirthday.strftime('%Y-%m-%d')
+                print("出生",uBirthday)
+            else:
+                uBirthday_str:str='1900-01-01'
 
             uAboutMe = form.uAboutMe.data
             print("自我介紹",uAboutMe)
@@ -74,7 +82,14 @@ def register():
             uPass = form.uPass.data
             print("密碼",uPass)
 
-            return redirect('/auth/login')
+            hash_password:str = generate_password_hash(uPass,method='pbkdf2:sha256',salt_length=8)
+            #print(hash_password)
+            #print("密碼正確" if check_password_hash(hash_password,uPass) else "密碼錯誤")
+
+            conn_token = secrets.token_hex(16)
+            
+            insert_data([uName,uGender,uPhone,uEmail,isGetEmail,uBirthday_str,uAboutMe,hash_password,conn_token])
+            return redirect(f'/auth/login/{uEmail}')
             
         else:
             print("驗證失敗")
